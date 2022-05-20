d178e0c6308686c3b48f1e0c9782c1d25f5edbb7
rbakhyshov <rbakhyshov@gmail.com>
2022-05-20 16:00:11 +0300

    v1

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = '5110614771:AAHxh_o-XxxQQsqYuMkkXq5xRas1NO9meX0'
-APP_TOKEN = 'https://induo.herokuapp.com/' + BOT_TOKEN
+BOT_T_1 = '5110614771'
+BOT_T_2 = f'{BOT_T_1}:AAHxh_o-XxxQQsqYuMkkXq5xRas1NO9meX0'
+APP_TOKEN = 'https://induo.herokuapp.com/' + BOT_T_2
